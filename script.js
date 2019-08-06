f42a20a14c6ea661a19b157c08bccad6650afeff
David Haxton <haxton.dc@gmail.com>
2019-08-06 11:39:19 -0700

    version

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BEEP BOOP MOTHERFUCKER
 // @author       d-haxton
-// @version      10.0
+// @version      10.1
 // @include      /^(https?:\/\/)?(www\.)?(.+)krunker\.io(|\/|\/\?(server|party|game)=.+)$/
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
